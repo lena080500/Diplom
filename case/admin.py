0a3b7e36b4431885473be18c98a67c8cea23a049
lena080500 <lena89507369144@gmail.com>
2022-04-19 23:43:33 +0500

    обновлены страницы для перехода на новые (Кейсы и Главную страницу)

--- case/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from case.models import Case, CaseParametr, CaseSection
-
-admin.site.register(Case)
-admin.site.register(CaseSection)
-admin.site.register(CaseParametr)
--- case/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CaseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'case'
-    verbose_name = 'Кейсы'
