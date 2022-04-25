7d28a78635f76ea5bca90c6f0daeaa695380ee5b
Melkorz <djeday2000a@yandex.ru>
2022-04-25 11:49:13 +0500

    Регистрация пользователей

--- case/views.py
+++ case/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-
 from .models import *
+from django.template import RequestContext
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 
 # Начальная функция вывода главной страницы.
-from django.template import RequestContext
-
-
 def main(request):
     cases = Case.objects.all()
     return render(
