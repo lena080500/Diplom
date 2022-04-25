634bef8ec7af1ea4b3d0a7374c4b786f5fb29e4a
Melkorz <djeday2000a@yandex.ru>
2022-04-25 11:54:05 +0500

    Регистрация пользователей

--- case/urls.py
+++ case/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),  # Начальная ссылка на функцию работы с данными моделей.
     path('case/', views.case, name='case'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login.as_view(), name='login'),
     path('contacts/', views.contacts, name='contacts'),
     path('search/', views.search, name='search'),
 ]
