97a599b1af06d17c4d880e90b51dd3cd99fa7ec0
lena080500 <lena89507369144@gmail.com>
2022-04-25 16:05:58 +0500

    case обновила и отображение на сайте.

--- case/views.py
+++ case/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
@@ -45,7 +46,7 @@ def search(request):
     )
 
 
-class login(CreateView):
+class Login(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     #template_name = 'registration/signup.html'
